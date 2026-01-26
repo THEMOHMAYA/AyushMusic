20fabce2ea5422380755ad9476d408d19126b7d9
MR MOH MAYA <124359102+THEMOHMAYA@users.noreply.github.com>
2026-01-26 12:31:12 +0530

    🥀ᴀʏᴜsʜ🥀

--- config.py
+++ config.py
@@ -10,14 +10,14 @@ API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OWNER_ID = int(os.getenv("OWNER_ID", None))
 OWNER_USERNAME = os.getenv("OWNER_USERNAME", "MOH_MAYA_OFFICIAL")
-BOT_USERNAME = os.getenv("BOT_USERNAME", "ShrutixMusicBot")
+BOT_USERNAME = os.getenv("BOT_USERNAME", "System_music_bot")
 
 MONGO_DB_URI = os.getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID", None))
 HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = os.getenv("HEROKU_API_KEY")
 
-UPSTREAM_REPO = os.getenv("UPSTREAM_REPO", "https://github.com/TEAM-NOBITA/Ayush-Music")
+UPSTREAM_REPO = os.getenv("UPSTREAM_REPO", "https://github.com/THEMOHMAYA/AyushMusic")
 UPSTREAM_BRANCH = os.getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = os.getenv("GIT_TOKEN", None)
 
